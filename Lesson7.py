488bb76ec30aee09afc8865137752d7ddb737a7b
ALEKSANDR BANNOV <45181936+KrezPro@users.noreply.github.com>
2018-11-21 22:29:10 +0300

    Seventh Lesson
    Lists

---
+++ Lesson7.py
@@ -0,0 +1,20 @@
+intlist = [12,21,32,42]
+charlist = ["a","b","c","d"]
+emptylist = []
+
+count = intlist[0:3]
+print(count)
+
+count2 = intlist[3]
+print(count2)
+
+count3 = intlist[::-1]
+print(count3)
+
+intlist.append(1)
+print(intlist)
+
+
+
+
+
